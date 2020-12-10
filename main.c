f59c124315d88ae332d067a46b3678de237692ca
eeliu <eeliu2009@gmail.com>
2020-12-10 15:53:22 +0800

    async

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
-
+#include "dlmalloc/malloc.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    #define _1_M 1*1024*1024
+    char* buffer = malloc(_1_M);
+    mspace *base = create_mspace_with_base(buffer,_1_M,1);
+    free(buffer);
     return 0;
 }
